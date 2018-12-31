b81343bfda06c2cf6592c50c0f6714ff2de3432a
soakes84 <spencer.oakes84@gmail.com>
2018-12-31 14:37:28 -0500

    moved into classes

--- MyTwitter-ishConsole/Program.cs
+++ MyTwitter-ishConsole/Program.cs
@@ -10,10 +10,9 @@ namespace MyTwitter_ishConsole
     {
         static void Main(string[] args)
         {
-            string choice;
-            string[] tweets = new string[10];
+            TweetsManager tm = new TweetsManager();
 
-            int tweetCount = 0;
+            string choice;  
 
             do
             {
@@ -28,23 +27,17 @@ namespace MyTwitter_ishConsole
 
                 if(choice == "1")
                 {
-                    for (int i = 0; i < tweetCount; i++)
-                        Console.WriteLine("Tweet #{0}: {1}", i + 1, tweets[i]);
+                    for (int i = 0; i < tm.TweetCount(); i++)
+                        Console.WriteLine("Tweet #{0}: {1}", i + 1, tm.GetTweets()[i]);
                     Console.WriteLine();
                 }
                 if(choice == "2")
                 {
                     Console.Write("Enter your tweet: ");
                     string tweet = Console.ReadLine();
-                    Console.WriteLine();
 
-                    if(tweet.Length > 280)
-                    {
-                        Console.WriteLine("Your tweet is over 280 characters and will not be saved.");
-                        continue;
-                    }
-
-                    tweets[tweetCount++] = tweet;
+                    string msg = tm.PostTweet(tweet);
+                    Console.WriteLine(msg);
                 }
 
                
---
+++ MyTwitter-ishConsole/TweetsManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTwitter_ishConsole
+{
+    class TweetsManager
+    {
+        string[] tweets = new string[10];
+
+        int tweetCount = 0;
+
+        public string PostTweet(string tweet)
+        {
+            if (tweet.Length > 280)
+                return "Tweets have to be 280 characters or less.";
+
+            if (tweetCount > tweets.Length)
+                return "You are only allowed 10 tweets. Sorry.";
+
+            tweets[tweetCount++] = tweet;
+
+            return "Your tweet has been posted.";
+        }
+
+        public string[] GetTweets()
+        {
+            return tweets;
+        }
+
+        public int TweetCount()
+        {
+            return tweetCount;
+        }
+    }
+}
