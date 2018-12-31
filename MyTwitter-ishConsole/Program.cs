846ace209a739d05c2c1f62ee34b9562fda2edde
soakes84 <spencer.oakes84@gmail.com>
2018-12-31 11:59:55 -0500

    long way to do it

--- MyTwitter-ishConsole/Program.cs
+++ MyTwitter-ishConsole/Program.cs
@@ -10,6 +10,31 @@ namespace MyTwitter_ishConsole
     {
         static void Main(string[] args)
         {
+            string[] tweets = new string[4];
+
+            Console.Write("Enter Your Tweet: ");
+            tweets[0] = Console.ReadLine();
+
+            Console.Write("Enter Second Tweet: ");
+            tweets[1] = Console.ReadLine();
+
+            Console.Write("Enter Third Tweet: ");
+            tweets[2] = Console.ReadLine();
+
+            Console.Write("Enter Fourth Tweet: ");
+            tweets[3] = Console.ReadLine();
+
+            Console.WriteLine("Your Tweets Posted Successfully.");
+            Console.WriteLine();
+
+            Console.WriteLine("Your Tweets Are: ");
+            Console.WriteLine(tweets[0]);
+            Console.WriteLine(tweets[1]);
+            Console.WriteLine(tweets[2]);
+            Console.WriteLine(tweets[3]);
+
+            Console.ReadLine();
+
         }
     }
 }
